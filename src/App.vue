3c72bd71cb683535c606361ac48a13b473dc4fa3
lixiaohu <1272662790@qq.com>
2023-06-30 09:47:25 +0800

    项目静态页面搭建完毕

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    app
+  <div class="container">
+    <HospitalTop />
+    <div class="content">
+      内容
+    </div>
+    <HospitalBottom />
   </div>
 </template>
 
@@ -9,5 +13,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .content{
+    margin-top: 70px;
+    width: 1200px;
+    min-height: 700px;
+  }
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import '@/style/reset.scss'
+import App from '@/App.vue'
+import HospitalTop from '@/components/hospital_top/index.vue'
+import HospitalBottom from '@/components/hospital_bottom/index.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .component('HospitalTop', HospitalTop)
+  .component('HospitalBottom', HospitalBottom)
+  .mount('#app')
