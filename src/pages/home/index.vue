6e05cd561c0bfc57cf2e42ac889b263e2bb84ca5
lixiaohu <1272662790@qq.com>
2023-06-30 10:55:17 +0800

    首页banner、搜索框静态搭建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <el-carousel height="350px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
+    <Carousel />
+    <Search />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import Carousel from "./carousel/index.vue";
+import Search from "./search/index.vue";
 </script>
 
 <style lang="scss" scoped>
